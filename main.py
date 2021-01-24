206522d8d27344e0b7dae8684748ea35674ae61f
Excalibur2667 <deeppatel2667@gmail.com>
2021-01-24 12:18:00 -0500

    Graphing the regression line.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 import PySimpleGUI as sg
-import matplotlib as plt
+import matplotlib.pyplot as plt
 """Regression Line 
    Gui to enter the points and gives the best fit line for the points.
    Calculations will be added later"""
 
+sg.theme('darkgrey9')
 layout=[[sg.Text("How many points would you like to enter")],           #First window, enter # of points
         [sg.Input(key='points',size=(40,1))],
         [sg.Button('Enter'),sg.Button('Exit')]]
@@ -12,40 +13,58 @@ layout=[[sg.Text("How many points would you like to enter")],           #First w
 def layout2(value):                                                     #Second window, enter each point
 
     layout2=[[sg.Text("Enter points   Column 1: X   Column 2: Y")]]
-    for i in range(value):
+    for i in range(1,value+1):
         layout2+=[[sg.Text("Point {}".format(i)), sg.Input(key='x{}'.format(i),size=(80,1)), sg.Input(key='y{}'.format(i),size=(80,1))]]
-    layout2+=[[sg.Button('Enter'),sg.Button('Exit')]]
+    layout2+=[[sg.Button('Enter'),sg.Button('Main Menu'),sg.Button('Exit')]]
     return layout2
 
-def insertscroll(layout2):                                              #Add a scroll for numbers not fitting on screen
+def insertscroll(layout3):                                              #Add a scroll for numbers not fitting on screen
 
-    layout2_scroll=[[sg.Column(layout2,scrollable=True,size=(1500,1500))]]
+    layout2_scroll=[[sg.Column(layout3,scrollable=True,size=(1500,1500))]]
     return layout2_scroll
 
 
 
-
-
-
-window=sg.Window('Enter points',layout).Finalize()
+window=sg.Window('Main Menu',layout).Finalize()
+x_values=[]
+y_values=[]
 while True:
     events, values = window.read()
     if(events==sg.WINDOW_CLOSED or events=='Exit'):
         break
+
+
     if(events=='Enter'):
-        number_of_points=int(float(values['points']))
-        layout2=layout2(number_of_points)
-        layout2_scroll = insertscroll(layout2)
-        window2=sg.Window("Enter points",layout2_scroll).Finalize()
+        window.hide()                                                       #Hide window
+        number_of_points=int((values['points']))
+        layout3=layout2(number_of_points)                                   #Adds the rows and columns to input points
+        layout3_scroll = insertscroll(layout3)                              #Adds scroll
+
+        window2=sg.Window("Enter points",layout3_scroll).Finalize()
         window2.maximize()
+
         events2, values2= window2.read()
 
-
         if(events2=='Exit' or events2==sg.WINDOW_CLOSED):
+            break
+
+        if(events2=='Main Menu'):                                           #Return to main menu
+            window.un_hide()
             window2.close()
 
-
-
+        if(events2=='Enter'):
+            for i in range(1,number_of_points+1):
+                x_values.append(int(values2['x{}'.format(i)]))
+                y_values.append(int(values2['y{}'.format(i)]))
+            window2.close()                                                 #Close all windows
+            break
 
 window.close()
-
+x=np.array(x_values)
+y=np.array(y_values)
+plt.plot(x,y,'bo')
+m, b = np.polyfit(x,y,1)
+plt.plot(x,m*x+b,'r',label="Best Fit Line")
+plt.title("Regression Line")
+plt.legend()
+plt.show()
